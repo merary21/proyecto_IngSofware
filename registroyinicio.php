624720e7ec8d81296071f34a943d601be41f599b
merary21 <julissa21araujo@gmail.com>
2023-11-28 21:36:08 -0600

    incrptar contraseña y agregar opcion de ayuda

--- registroyinicio.php
+++ registroyinicio.php
@@ -7,14 +7,17 @@ $telefono = $_POST['telefono'];
 $usuario = $_POST['usuario'];
 $contra = $_POST['contra'];
 
+// Encripta la contraseña
+$hashed_password = password_hash($contra, PASSWORD_DEFAULT);
+
 $query = "INSERT INTO usuariopaciente(nombre_completo, correo, telefono, usuario, contra)
-VALUES('$nombre_completo', '$correo', '$telefono', '$usuario', '$contra')";
+VALUES('$nombre_completo', '$correo', '$telefono', '$usuario', '$hashed_password')";
 
 $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuariopaciente WHERE correo='$correo'");
-if (mysqli_num_rows($verificar_correo)> 0) {
+if (mysqli_num_rows($verificar_correo) > 0) {
     echo '
     <script>
-    alert("Este correo ya esta registrado. Intenta con otro diferente");
+    alert("Este correo ya está registrado. Intenta con otro diferente");
     window.location = "registro.php";
     </script>
     ';
@@ -22,32 +25,32 @@ if (mysqli_num_rows($verificar_correo)> 0) {
 }
 
 $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuariopaciente WHERE usuario='$usuario'");
-if (mysqli_num_rows($verificar_usuario)> 0) {
+if (mysqli_num_rows($verificar_usuario) > 0) {
     echo '
     <script>
-    alert("Este usuario ya esta registrado. Intenta con otro diferente");
+    alert("Este usuario ya está registrado. Intenta con otro diferente");
     window.location = "registro.php";
     </script>
     ';
     exit();
 }
 
-    $ejecutar = mysqli_query($conexion, $query);
-if($ejecutar){
+$ejecutar = mysqli_query($conexion, $query);
+if ($ejecutar) {
     echo '
     <script>
     alert("Usuario registrado exitosamente");
     window.location = "menu.php";
     </script>
     ';
-
-}else {
+} else {
     echo '
     <script>
-    alert("Usuario no registrado, intentelo nuevamente");
+    alert("Usuario no registrado, inténtelo nuevamente");
     window.location = "registro.php";
     </script>
     ';
 }
-mysqli_close($conexion); 
+
+mysqli_close($conexion);
 ?>
--- views/citas.php
+++ views/citas.php
@@ -1,11 +1,9 @@
 <?php
-// Seguridad de sesiones
 session_start();
 error_reporting(0);
 $varsesion = $_SESSION['nombre'];
 
-if ($varsesion == null || $varsesion = '') {
-
+if ($varsesion == null || $varsesion == '') {
     header("Location: ../includes/_sesion/login.php");
     die();
 }
@@ -22,140 +20,69 @@ if ($varsesion == null || $varsesion = '') {
     <meta name="description" content="">
     <meta name="author" content="">
 
+    <!-- Enlaces a Bootstrap y tu archivo de estilos CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/estilos.css"> <!-- Asegúrate de ajustar la ruta al archivo CSS correcta -->
+
     <script src="../js/jquery.min.js"></script>
 
 </head>
+
 <?php include "../includes/header.php"; ?>
-
-
 
 <body id="page-top">
 
-    <!-- Begin Page Content -->
-    <div class="container-fluid">
+    <div class="container-fluid mt-4">
 
+        <?php
+        include "../includes/db.php";
+        // Mostrar citas registradas
+        $resultado_mostrar = mysqli_query($conexion, "SELECT * FROM citas");
+        ?>
 
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Lista de Citas</h6>
-                <br>
-                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#citas">
-                    <span class="glyphicon glyphicon-plus"></span> Agregar cita <i class="fa fa-plus-circle" aria-hidden="true"></i> </a></button>
-
-            </div>
-
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>NumCita#</th>
-                                <th>Fecha_Cita</th>
-                                <th>Horario</th>
-                                <th>Paciente</th>
-                                <th>Doctor</th>
-                                <th>Especialidad</th>
-                                <th>Observacion</th>
-                                <th>Estado</th>
-                                <th>Fecha_Registro</th>
-                                <th>Acciones..</th>
-                            </tr>
-                        </thead>
-
-                        <?php
-
-                        include "../includes/db.php";
-                        $result = mysqli_query($conexion, "SELECT c.id, c.fecha, c.hora, c.id_paciente, 
-                        c.id_doctor, c.id_especialidad, c.observacion, c.fecha_registro, e.estado FROM citas c 
-                        LEFT JOIN estado e ON c.estado= e.id  ");
-                        while ($fila = mysqli_fetch_assoc($result)) :
-
-                        ?>
+        <?php if ($resultado_mostrar) : ?>
+            <h2 class="mb-4">Citas Registradas:</h2>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>Fecha</th>
+                            <th>Hora</th>
+                            <th>ID Doctor</th>
+                            <th>ID Especialidad</th>
+                            <th>Nombre Paciente</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($fila = mysqli_fetch_assoc($resultado_mostrar)) : ?>
                             <tr>
                                 <td><?php echo $fila['id']; ?></td>
                                 <td><?php echo $fila['fecha']; ?></td>
                                 <td><?php echo $fila['hora']; ?></td>
-                                <td><?php echo $fila['id_paciente']; ?></td>
                                 <td><?php echo $fila['id_doctor']; ?></td>
                                 <td><?php echo $fila['id_especialidad']; ?></td>
-                                <td><?php echo $fila['observacion']; ?></td>
-                                <td><?php echo $fila['estado']; ?></td>
-                                <td><?php echo $fila['fecha_registro']; ?></td>
-
-                                <td>
-                                    <a class="btn btn-warning" href="../includes/editar_cita.php?id=<?php echo $fila['id'] ?> ">
-                                        <i class="fa fa-edit "></i> </a>
-                                    <a href="../includes/eliminar_cita.php?id=<?php echo $fila['id'] ?> " class="btn btn-danger btn-del">
-                                        <i class="fa fa-trash "></i></a></button>
-                                </td>
+                                <td><?php echo $fila['nombre_paciente']; ?></td>
                             </tr>
-
-
                         <?php endwhile; ?>
-
-                        </tbody>
-                    </table>
-
-
-                    <script>
-                        $('.btn-del').on('click', function(e) {
-                            e.preventDefault();
-                            const href = $(this).attr('href')
-
-                            Swal.fire({
-                                title: 'Estas seguro de eliminar esta cita?',
-                                text: "¡No podrás revertir esto!!",
-                                icon: 'warning',
-                                showCancelButton: true,
-                                confirmButtonColor: '#3085d6',
-                                cancelButtonColor: '#d33',
-                                confirmButtonText: 'Si, eliminar!',
-                                cancelButtonText: 'Cancelar!',
-                            }).then((result) => {
-                                if (result.value) {
-                                    if (result.isConfirmed) {
-                                        Swal.fire(
-                                            'Eliminado!',
-                                            'El usuario fue eliminado.',
-                                            'success'
-                                        )
-                                    }
-
-                                    document.location.href = href;
-                                }
-                            })
-
-                        })
-                    </script>
-                    <script src="../package/dist/sweetalert2.all.js"></script>
-                    <script src="../package/dist/sweetalert2.all.min.js"></script>
-                    <script src="../package/jquery-3.6.0.min.js"></script>
-
-
-
-                </div>
+                    </tbody>
+                </table>
             </div>
-        </div>
+        <?php else : ?>
+            <p class="alert alert-danger">Error al obtener las citas: <?php echo mysqli_error($conexion); ?></p>
+        <?php endif; ?>
+
+        <?php
+        mysqli_close($conexion);
+        ?>
+
+        <script src="../package/dist/sweetalert2.all.js"></script>
+        <script src="../package/dist/sweetalert2.all.min.js"></script>
+        <script src="../package/jquery-3.6.0.min.js"></script>
 
     </div>
-    <!-- /.container-fluid -->
-
-    </div>
-    <!-- End of Main Content -->
 
     <?php include "../includes/footer.php"; ?>
-
-    <?php include "../includes/form_cita.php"; ?>
-
-    </div>
-    <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-
-
 
 </body>
 
